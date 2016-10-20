5f43a8f22be0d7075d007ff4224a4f49ff1ba64f
Ryan Travitz <rtravitz@gmail.com>
2016-10-20 17:42:09 -0600

    Add model tests for category and update for job

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a title" do
+        category = Category.new
+        expect(category).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a title" do
+        category = Category.new(title: "Developer")
+        expect(category).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs" do
+      category = Category.new(title: "Developer")
+      expect(category).to respond_to(:jobs)
+    end
+  end
 end
