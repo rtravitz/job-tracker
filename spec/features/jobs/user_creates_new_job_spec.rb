c2753732eef34b73bd06d491783d60b530ce462d
Ryan Travitz <rtravitz@gmail.com>
2016-10-21 16:40:12 -0600

    Complete categories

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -5,6 +5,8 @@ describe "User creates a new job" do
     company = Company.create(name: "ESPN", city: "NYC")
     category1, category2, category3 = create_list(:category, 3)
     visit new_company_job_path(company)
+    save_and_open_page
+    expect(page).to have_selector(:link_or_button, "Create New Category")
 
     fill_in "job[title]", with: "Developer"
     fill_in "job[description]", with: "So fun!"
