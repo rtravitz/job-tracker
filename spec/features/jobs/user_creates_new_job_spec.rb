ff4cc1a1acb1440bc693d08dab2e72f2fafa8f43
Ryan Travitz <rtravitz@gmail.com>
2016-10-21 16:40:49 -0600

    Complete categories and removed save and open page

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -5,7 +5,6 @@ describe "User creates a new job" do
     company = Company.create(name: "ESPN", city: "NYC")
     category1, category2, category3 = create_list(:category, 3)
     visit new_company_job_path(company)
-    save_and_open_page
     expect(page).to have_selector(:link_or_button, "Create New Category")
 
     fill_in "job[title]", with: "Developer"
