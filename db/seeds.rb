d81c17ac858535a61bd2c5ee7d8cd88575b5de84
Ryan Travitz <rtravitz@gmail.com>
2016-10-22 14:23:24 -0600

    Fix seeds to include categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,15 @@ Job.destroy_all
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+CATEGORIES = ["Tech","Education","Consulting", "Marketing"]
+CATEGORY_KEYS = [1, 1, 1, 1, 2, 1, 3, 4] 
+CATEGORIES.each do |title|
+  Category.create!(title: title)
+end
 
 COMPANIES.each do |name|
   company = Company.create!(name: name, city: CITIES.sample)
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100))
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), category_id: CATEGORY_KEYS.sample)
   end
 end
