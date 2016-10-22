5301d310cfebd10b43a5520e68121b0144730260
Ryan Travitz <rtravitz@gmail.com>
2016-10-22 12:31:30 -0600

    Add ability for user to create contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ActiveRecord::Base
-  validates :full_name, :email, :company_id, presence: true
+  validates :full_name, :position, :email, :company_id, presence: true
   belongs_to :company
 end
